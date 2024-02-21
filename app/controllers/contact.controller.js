d3f9b495972d47b97a4973148c581d6f26bde857
b2016975 <khoib2016975@student.ctu.edu.vn>
2024-02-21 10:22:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create = (req, res) => {
+    res.send({ messsage: "create handler" });
+};
+exports.findAll = (req, res) => {
+    res.send({ messsage: "findAll handler" });
+};
+exports.findOne = (req, res) => {
+    res.send({ messsage: "findOne handler" });
+};
+exports.update = (req, res) => {
+    res.send({ messsage: "update handler" });
+};
+exports.delete = (req, res) => {
+    res.send({ messsage: "delete handler" });
+};
+exports.deleteAll = (req, res) => {
+    res.send({ messsage: "deleteAll handler" });
+};
+exports.findAllFavorite = (req, res) => {
+    res.send({ messsage: "findAllFavorite handler" });
+};
